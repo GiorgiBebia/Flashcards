7bea8eb931491932e9a855a925160e4db9690e7b
GiorgiBebia <139773975+GiorgiBebia@users.noreply.github.com>
2024-08-25 13:29:39 +0400

    Flashcards

---
+++ src/questions.js
@@ -0,0 +1,33 @@
+export const questions = [
+  {
+    id: 3457,
+    question: "What Language is React Based On?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What Are The Building Blocks of React Apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's The Name of The Syntax We Use To Describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How To Pass Data From Parent To Child Components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How To Give Components Memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What Do We Call An Input Element That is Completely Synchronised With State?",
+    answer: "Controlled Element",
+  },
+];
